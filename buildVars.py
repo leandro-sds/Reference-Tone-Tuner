f57e72e8886facff07df5ebe4812c8d9d99f99d9
Leandro Souza <59955744+leandro-sds@users.noreply.github.com>
2026-03-16 09:15:58 -0300

    2026.03.16
    Primeira versão publica do Reference Tone Tuner

--- buildVars.py
+++ buildVars.py
@@ -39,7 +39,7 @@ It can span multiple lines."""),
 	# Documentation file name
 	addon_docFileName="readme.html",
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
-	addon_minimumNVDAVersion="2024.4.1",
+	addon_minimumNVDAVersion="2024.1.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
 	addon_lastTestedNVDAVersion="2025.3.3",
 	# Add-on update channel (default is None, denoting stable releases,
