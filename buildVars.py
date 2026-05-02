deb5d012e5e24ee6d6cdf4b36d7c67c844f71e1c
Leandro Souza <59955744+leandro-sds@users.noreply.github.com>
2026-05-01 21:43:58 -0300

    2026.1
    Atualizada a compatibilidade para NVDA 2026.1

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = AddonInfo(
 Use as teclas 1 a 6 para tocar as cordas e afine de ouvido até coincidir.
 Pressione F1 para ajuda e R para repetir em loop."""),
 	# version
-	addon_version="2026.03.16",
+	addon_version="2026.05.02",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""Changelog for the add-on version.
@@ -41,7 +41,7 @@ It can span multiple lines."""),
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2024.1.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.3.3",
+	addon_lastTestedNVDAVersion="2026.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
--- changelog.md
+++ changelog.md
@@ -1,6 +1,16 @@
 # Changelog
 
-## [2026.03.16] - Primeira versão pública
-- Lançamento inicial do **Reference Tone-uner**, um complemento para o NVDA.
-- Afinador de violão acessível para pessoas cegas.
-- O programa reproduz o som de cada corda, permitindo que o usuário ajuste a afinação de ouvido.
+
+
+## \[2026.05.02]
+
+* Atualizada a compatibilidade para NVDA 2026.1
+
+
+
+## \[2026.03.16] - Primeira versão pública
+
+* Lançamento inicial do **Reference Tone-uner**, um complemento para o NVDA.
+* Afinador de violão acessível para pessoas cegas.
+* O programa reproduz o som de cada corda, permitindo que o usuário ajuste a afinação de ouvido.
+
